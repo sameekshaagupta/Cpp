3de48defdf3260435222eebbb28e3cdd6880299d
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-07-25 04:15:36 +0530

    cout

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(){
+    cout << "Sameekssha";
+}
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+    print();
+    int s = sum(1, 5);
+    cout << s;
+    return 0;
+}
